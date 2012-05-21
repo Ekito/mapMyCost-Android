3610f0f7a57c5cadaf8153f4cee726b9e3caf48c
Romain Piel <romain.piel@gmail.com>
2012-05-21 10:17:14 +0200

    added zubhium sdk

--- src/com/ekito/mapmycost/MMCApplication.java
+++ src/com/ekito/mapmycost/MMCApplication.java
@@ -1,5 +1,8 @@
 package com.ekito.mapmycost;
 
+import com.zubhium.ZubhiumSDK;
+import com.zubhium.ZubhiumSDK.CrashReportingMode;
+
 import android.app.Application;
 import android.httpimage.FileSystemPersistence;
 import android.httpimage.HttpImageManager;
@@ -12,6 +15,11 @@ public class MMCApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 
+		// init zubhium
+		ZubhiumSDK sdk = ZubhiumSDK.getZubhiumSDKInstance(this, "2a1ae0d4b7bd023858cc8518858694");
+		//  overide the ugly Force Close message and improve user experience
+		sdk.setCrashReportingMode(CrashReportingMode.SILENT);
+		
 		// init HttpImageManager manager.
 		mHttpImageManager = new HttpImageManager(new FileSystemPersistence(BASEDIR));
 	}
